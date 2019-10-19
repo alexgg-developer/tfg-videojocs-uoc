5b66257ed445ecab3345054eb8955a6f77d70ed6
Alex <alexgg.developer@gmail.com>
2019-10-19 09:16:47 +0200

    Afegit la escena de càrrega i el codi per carregar escenes de forma asíncrona. També una font.

---
+++ Assets/Scripts/Loading/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+
+    public void Awake()
+    {
+        LoadLevel(PlayerPrefs.GetInt("SceneToLoad"));
+    }
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while(!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
